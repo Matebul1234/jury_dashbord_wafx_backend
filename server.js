58bc9dfbd3fddf24b237f5618c431a853839d3e3
Matebul1234 <113576114+Matebul1234@users.noreply.github.com>
2024-11-26 15:34:05 +0530

    packej_json
    I have only change in json file

--- server.js
+++ server.js
@@ -20,20 +20,20 @@ app.use(cors({
 }));
 
 // db connected 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "root123",
-//     database: "abai"
-// });
-
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: "localhost",
+    user: "root",
+    password: "root123",
+    database: "abai"
 });
+
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
 
 
 
